1f0d2670bdfac4ab446619c41ce700337d967257
shuaiyafei <shuaiyafei@shuidihuzhu.com>
2019-07-22 20:24:59 +0800

    u

---
+++ javascript/show-image-content-size.js
@@ -0,0 +1,1 @@
+alert('show-image-content-size');
--- javascript/warden.js
+++ javascript/warden.js
@@ -1,1 +1,20 @@
-alert(1);
+console.table(`%c%s%c%s%c%s`,
+    `
+    color: lightgray;
+    font-size: 16px;
+    `,
+    `
+    Justice will be done
+    `,
+    `color: red;
+    font-size: 16px;`,
+    `
+    We're wasting time here
+    `,
+    `
+    color: purple;
+    font-size: 16px;
+    `,
+    `
+    Illidan is out ther somewhere
+    `);
